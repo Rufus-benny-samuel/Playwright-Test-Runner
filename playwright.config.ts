721ff37d47af1b682cb9f73208ee6cc8b8a420e0
rrufusbenny <rrufusbenny@gmail.com>
2023-10-10 08:11:05 +0530

    test(common): headless

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ const config: PlaywrightTestConfig = {
 
     use: {
         viewport: null,
-        headless: !true,
+        headless: true,
         // browserName: "chromium",
         screenshot: "on",
         video: "on",
